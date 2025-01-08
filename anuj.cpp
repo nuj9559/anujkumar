7f738eb2965b80da6fe603906c1d6731da7348db
nuj9559 <anujkumar773849@gmail.com>
2025-01-08 16:20:35 +0530

    Adding files to my GitHub repo

---
+++ anuj.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"\n Hello world";
+    return 0;
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Example usage
+const number1 = 5; // First number
+const number2 = 10; // Second number
+
+const sum = addNumbers(number1, number2);
+console.log(`The sum of ${number1} and ${number2} is: ${sum}`);
